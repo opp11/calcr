50105e5a959f51ee4c8b28736cb1eeb3f330abd9
Patrick M Jensen <patmjen@gmail.com>
2015-04-05 18:06:25 +0200

    Add a continuous calculator to the program
    If no equations are given at startup, then the program will
    continously prompt the user for an equation to evaluate.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate getopts;
 
 use std::env;
+use std::io::Write;
 use getopts::Options;
 use errors::CalcrResult;
 
@@ -38,12 +39,37 @@ fn main() {
 				Err(e) => e.print(Some(&eq)),
 			}
 		}
-	}
+	} else {
+        run_enviroment();
+    }
 }
 
 fn process_eq(eq: &String) -> CalcrResult<f64> {
 	let ast = try!(parser::parse_equation(&eq));
 	evaluator::eval_eq(&ast)
+}
+
+fn run_enviroment() {
+    print_version();
+    let mut buf = String::new();
+    let mut in_stream = std::io::stdin();
+    let mut out_stream = std::io::stdout();
+    loop {
+        print!(">> ");
+        // we explicitly call flush on stdout, or else the '>>' prompt won't be printed untill
+        // after we have read the user's input
+        out_stream.flush().ok().expect("Fatal error while writing prompt");
+        in_stream.read_line(&mut buf).ok().expect("Fatal error while reading input");
+        let eq = buf.trim().to_string();
+        if eq == "quit" {
+            break;
+        }
+        match process_eq(&eq) {
+            Ok(num) => println!("{}", num),
+            Err(e) => e.print(Some(&eq)),
+        }
+        buf.clear();
+    }
 }
 
 fn print_usage(opts: Options) {
