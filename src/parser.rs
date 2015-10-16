db0decadbedf006d10c588fe3c76f69ace65ca7e
Patrick M. Jensen <patmjen@gmail.com>
2015-10-16 16:53:53 +0200

    Revamp peek_tok_val to return a reference
    And as result, as lot of the parser code had to be changed as
    well. It could still use some optimization though.

--- src/parser.rs
+++ src/parser.rs
@@ -59,8 +59,8 @@ pub struct Parser {
 impl Parser {
     fn parse_equation(&mut self) -> CalcrResult<Ast> {
         let mut lhs = try!(self.parse_term());
-        while self.peek_tok_val() == Some(TokVal::Op(Plus)) ||
-              self.peek_tok_val() == Some(TokVal::Op(Minus)) {
+        while self.peek_tok_val().map_or(false, |val| *val == TokVal::Op(Plus) ||
+                                                      *val == TokVal::Op(Minus)) {
             let Token { val: tok_val, span: tok_span } = self.consume_tok();
             let rhs = try!(self.parse_term());
             lhs = Ast {
@@ -69,13 +69,15 @@ impl Parser {
                 branches: Binary(Box::new(lhs), Box::new(rhs)),
             }
         }
-        if self.peek_tok_val() == Some(TokVal::ParenClose) && self.paren_level < 1 {
+        if self.peek_tok_val().map_or(false, |val| *val == TokVal::ParenClose) &&
+           self.paren_level < 1 {
             let Token { val: _, span: tok_span } = self.consume_tok();
             Err(CalcrError {
                 desc: format!("Missing opening parentheses"),
                 span: Some(tok_span),
             })
-        } else if self.peek_tok_val() == Some(TokVal::AbsDelim) && self.abs_level < 1 {
+        } else if self.peek_tok_val().map_or(false, |val| *val == TokVal::AbsDelim) &&
+                  self.abs_level < 1 {
             let Token { val: _, span: tok_span } = self.consume_tok();
             Err(CalcrError {
                 desc: format!("Missing opening abs delimiter"),
@@ -90,17 +92,17 @@ impl Parser {
         // check if we have a function
         let func_opt = match self.peek_tok_val() {
             // TODO: Make this not horrible, since the compiler kept bugging me
-            Some(TokVal::Name(ref name)) if *name == "cos".to_string() => Some(Cos),
-            Some(TokVal::Name(ref name)) if *name == "sin".to_string() => Some(Sin),
-            Some(TokVal::Name(ref name)) if *name == "tan".to_string() => Some(Tan),
-            Some(TokVal::Name(ref name)) if *name == "asin".to_string() => Some(Asin),
-            Some(TokVal::Name(ref name)) if *name == "acos".to_string() => Some(Acos),
-            Some(TokVal::Name(ref name)) if *name == "atan".to_string() => Some(Atan),
-            Some(TokVal::Name(ref name)) if *name == "sqrt".to_string() => Some(Sqrt),
-            Some(TokVal::Name(ref name)) if *name == "abs".to_string() => Some(Abs),
-            Some(TokVal::Name(ref name)) if *name == "exp".to_string() => Some(Exp),
-            Some(TokVal::Name(ref name)) if *name == "ln".to_string() => Some(Ln),
-            Some(TokVal::Name(ref name)) if *name == "log".to_string() => Some(Log),
+            Some(&TokVal::Name(ref name)) if *name == "cos".to_string() => Some(Cos),
+            Some(&TokVal::Name(ref name)) if *name == "sin".to_string() => Some(Sin),
+            Some(&TokVal::Name(ref name)) if *name == "tan".to_string() => Some(Tan),
+            Some(&TokVal::Name(ref name)) if *name == "asin".to_string() => Some(Asin),
+            Some(&TokVal::Name(ref name)) if *name == "acos".to_string() => Some(Acos),
+            Some(&TokVal::Name(ref name)) if *name == "atan".to_string() => Some(Atan),
+            Some(&TokVal::Name(ref name)) if *name == "sqrt".to_string() => Some(Sqrt),
+            Some(&TokVal::Name(ref name)) if *name == "abs".to_string() => Some(Abs),
+            Some(&TokVal::Name(ref name)) if *name == "exp".to_string() => Some(Exp),
+            Some(&TokVal::Name(ref name)) if *name == "ln".to_string() => Some(Ln),
+            Some(&TokVal::Name(ref name)) if *name == "log".to_string() => Some(Log),
             _ => None,
         };
 
@@ -119,8 +121,8 @@ impl Parser {
 
     fn parse_product(&mut self) -> CalcrResult<Ast> {
         let mut lhs = try!(self.parse_factor());
-        while self.peek_tok_val() == Some(TokVal::Op(Mult)) ||
-              self.peek_tok_val() == Some(TokVal::Op(Div)) {
+        while self.peek_tok_val().map_or(false, |val| *val == TokVal::Op(Mult) ||
+                                                      *val == TokVal::Op(Div)) {
             let Token { val: tok_val, span: tok_span } = self.consume_tok();
             let rhs = try!(self.parse_term());
             lhs = Ast {
@@ -135,7 +137,7 @@ impl Parser {
     fn parse_factor(&mut self) -> CalcrResult<Ast> {
         // when we lex we only store `Minus`s since we do not have any context there,
         // however we know if we see a `Minus` now, then it is a `Neg`.
-        if self.peek_tok_val() == Some(TokVal::Op(Minus)) {
+        if self.peek_tok_val().map_or(false, |val| *val == TokVal::Op(Minus)) {
             let tok_span = self.consume_tok().span;
             let rhs = try!(self.parse_factor());
             Ok(Ast {
@@ -145,7 +147,7 @@ impl Parser {
             })
         } else {
             let lhs = try!(self.parse_exponent());
-            if self.peek_tok_val() == Some(TokVal::Op(Pow)) {
+            if self.peek_tok_val().map_or(false, |val| *val == TokVal::Op(Pow)) {
                 let tok_span = self.consume_tok().span;
                 let rhs = try!(self.parse_factor());
                 Ok(Ast {
@@ -162,7 +164,7 @@ impl Parser {
     fn parse_exponent(&mut self) -> CalcrResult<Ast> {
         let mut out = try!(self.parse_number());
 
-        while self.peek_tok_val() == Some(TokVal::Op(Fact)) {
+        while self.peek_tok_val().map_or(false, |val| *val == TokVal::Op(Fact)) {
             let tok_span = self.consume_tok().span;
             out = Ast {
                 val: AstVal::Op(Fact),
@@ -174,89 +176,90 @@ impl Parser {
     }
 
     fn parse_number(&mut self) -> CalcrResult<Ast> {
-        match self.peek_tok_val() {
-            Some(TokVal::ParenOpen) => {
-                // store the current pos in case we need to report a paren error
-                let open_paren_span = self.consume_tok().span;
-                self.paren_level += 1;
-                let eq = try!(self.parse_equation());
-
-                if self.peek_tok_val() != Some(TokVal::ParenClose) {
-                    Err(CalcrError {
-                        desc: "Missing closing parentheses".to_string(),
-                        span: Some(open_paren_span),
-                    })
-                } else {
-                    self.paren_level -= 1;
-                    let close_paren_span = self.consume_tok().span;
-                    Ok(Ast {
-                        val: AstVal::Paren,
-                        span: (open_paren_span.0, close_paren_span.1),
-                        branches: Unary(Box::new(eq)),
-                    })
-                }
-            },
-            Some(TokVal::AbsDelim) => {
-                // store the current pos in case we need to report a paren error
-                let open_delim_span = self.consume_tok().span;
-                self.abs_level += 1;
-                let eq = try!(self.parse_equation());
-
-                if self.peek_tok_val() != Some(TokVal::AbsDelim) {
-                    Err(CalcrError {
-                        desc: "Missing closing abs delimiter".to_string(),
-                        span: Some(open_delim_span),
-                    })
-                } else {
-                    self.abs_level -= 1;
-                    let close_delim_span = self.consume_tok().span;
-                    Ok(Ast {
-                        val: AstVal::Func(Abs),
-                        span: (open_delim_span.0, close_delim_span.1),
-                        branches: Unary(Box::new(eq)),
-                    })
-                }
-            },
-            Some(TokVal::Name(ref name)) => {
-                // at this point any Name, HAS to be a known constant
-                let cnst = match name.as_ref() {
-                    "pi" => Pi,
-                    "e" => E,
-                    "phi" => Phi,
-                    _ => return Err(CalcrError {
-                        desc: format!("Invalid function or constant: {}", name),
-                        span: Some(self.consume_tok().span),
-                    }),
-                };
-                let span = self.consume_tok().span;
-                Ok(Ast {
-                    val: AstVal::Const(cnst),
-                    span: span,
-                    branches: Leaf,
-                })
-            },
-            Some(TokVal::Num(num)) => {
-                let span = self.consume_tok().span;
-                Ok(Ast {
-                    val: AstVal::Num(num),
-                    span: span,
-                    branches: Leaf,
-                })
-            },
-            Some(_) => Err(CalcrError {
-                desc: format!("Expected number or constant"),
-                span: Some(self.consume_tok().span),
-            }),
-            None => Err(CalcrError {
+        if self.toks_empty() {
+            Err(CalcrError {
                 desc: format!("Expected number or constant"),
                 span: Some((self.end_pos, self.end_pos)),
-            }),
+            })
+        } else {
+            let Token { val: tok_val, span: tok_span } = self.consume_tok();
+            match tok_val {
+                TokVal::ParenOpen => {
+                    self.paren_level += 1;
+                    let eq = try!(self.parse_equation());
+                    if self.peek_tok_val().map_or(false, |val| *val != TokVal::ParenClose) {
+                        Err(CalcrError {
+                            desc: "Missing closing parentheses".to_string(),
+                            span: Some(tok_span),
+                        })
+                    } else {
+                        self.paren_level -= 1;
+                        let close_paren_span = self.consume_tok().span;
+                        Ok(Ast {
+                            val: AstVal::Paren,
+                            span: (tok_span.0, close_paren_span.1),
+                            branches: Unary(Box::new(eq)),
+                        })
+                    }
+                },
+                TokVal::AbsDelim => {
+                    self.abs_level += 1;
+                    let eq = try!(self.parse_equation());
+                    if self.peek_tok_val().map_or(false, |val| *val != TokVal::AbsDelim) {
+                        Err(CalcrError {
+                            desc: "Missing closing abs delimiter".to_string(),
+                            span: Some(tok_span),
+                        })
+                    } else {
+                        self.abs_level -= 1;
+                        let close_delim_span = self.consume_tok().span;
+                        Ok(Ast {
+                            val: AstVal::Func(Abs),
+                            span: (tok_span.0, close_delim_span.1),
+                            branches: Unary(Box::new(eq)),
+                        })
+                    }
+                },
+                TokVal::Name(ref name) => {
+                    // at this point any Name, HAS to be a known constant
+                    let cnst = match name.as_ref() {
+                        "pi" => Pi,
+                        "e" => E,
+                        "phi" => Phi,
+                        _ => return Err(CalcrError {
+                            desc: format!("Invalid function or constant: {}", name),
+                            span: Some(tok_span),
+                        }),
+                    };
+                    Ok(Ast {
+                        val: AstVal::Const(cnst),
+                        span: tok_span,
+                        branches: Leaf,
+                    })
+                },
+                TokVal::Num(num) => {
+                    Ok(Ast {
+                        val: AstVal::Num(num),
+                        span: tok_span,
+                        branches: Leaf,
+                    })
+                },
+                _ => Err(CalcrError {
+                    desc: format!("Expected number or constant"),
+                    span: Some(tok_span),
+                }),
+            }
         }
     }
 
+    /// Checks if we have run out of `Token`s to parse
+    fn toks_empty(&mut self) -> bool {
+        self.iter.peek().is_none()
+    }
+
     /// Peeks at the next `Token` and returns `Some` if one was found, or `None` if none are left
-    fn peek_tok_val(&mut self) -> Option<TokVal> {
-        self.iter.peek().and_then(|ref tok| Some(tok.val.clone()))
+    fn peek_tok_val(&mut self) -> Option<&TokVal> {
+        self.iter.peek().and_then(|ref tok| Some(&tok.val))
     }
 
     /// Consumes a `Token` - thereby advanding `pos` - and returns it
