7d0a736589583d999fc1853c744ae3fac0750fbe
Patrick M. Jensen <patmjen@gmail.com>
2016-01-03 14:11:43 +0100

    Bump to version v0.6.1

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod token;
 mod input;
 
 const PROG_NAME: &'static str = "calcr";
-const VERSION: &'static str = "v0.6.0";
+const VERSION: &'static str = "v0.6.1";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
